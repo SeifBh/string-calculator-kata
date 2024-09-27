afda40a52e06bfa49ca8b0c34de17d6a5f1bb579
BEL HADJ ALI SeifEddine <belhadjali.seif@gmail.com>
2024-09-27 15:21:13 +0200

    a

---
+++ src/main/java/StringCalculatorStep7.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class StringCalculatorStep7 {
+    public static final int Add(final String numbers) {
+        int returnValue = 0;
+        String[] numbersArray = numbers.split(",");
+        if (numbersArray.length > 2) {
+            throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
+
+        } else {
+            if (!numbers.isEmpty()) {
+                returnValue = Arrays.stream(numbersArray).mapToInt(Integer::parseInt).sum();
+            }
+        }
+        return returnValue;
+    }
+}
