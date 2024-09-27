d2df8f05b9a37773b9e40e16571a4382d5f7f052
BEL HADJ ALI SeifEddine <belhadjali.seif@gmail.com>
2024-09-27 20:17:14 +0200

    Ajouter dépendance org.projectlombok/lombok 1.18.30 en scope provided.

--- src/main/java/ChatRequest.java
+++ src/main/java/ChatRequest.java
@@ -1,27 +1,161 @@
-import lombok.*;
+diff --git a/pom.xml b/pom.xml
+index aeda49e..31cc441 100644
 
-import java.util.List;
 
-/**
- * @author madhankumar
- */
-@Setter
-@Getter
-@ToString
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class ChatRequest {
-    private String model;
-    private List<?> messages;
-    private int n;
-    private double temperature;
 
-    public boolean newMethod(String st1, String st2) {
-        if (st1 == st2) {
-            return true;
-        }
-        return false;
-    }
-}
+--- a/pom.xml
++++ b/pom.xml
+@@ -15,6 +15,12 @@
+             <version>4.13</version>
+
+
+
+             <scope>test</scope>
+
+
+
+        </dependency>
+
+
+
++        <dependency>
+
+
+
++            <groupId>org.projectlombok</groupId>
+
+
+
++            <artifactId>lombok</artifactId>
+
+
+
++            <version>1.18.30</version>
+
+
+
++            <scope>provided</scope>
+
+
+
++        </dependency>
+
+
+
+        </dependencies>
+
+
+
+
+     <properties>
+
+
+
+diff --git a/src/main/java/ChatRequest.java b/src/main/java/ChatRequest.java
+new file mode 100644
+
+
+
+index 0000000..c01b71d
+--- /dev/null
+
+
+
++++ b/src/main/java/ChatRequest.java
+@@ -0,0 +1,27 @@
++import lombok.*;
+
+
+
++
+
+
+
++import java.util.List;
+
+
+
++
+
+
+
++/**
+ 
+
+
+ + * @author madhankumar
+ 
+
+
+ + */
+
+
+
++@Setter
+
+
+
++@Getter
+
+
+
++@ToString
+
+
+
++@Data
+
+
+
++@Builder
+
+
+
++@NoArgsConstructor
+
+
+
++@AllArgsConstructor
+
+
+
++public class ChatRequest {
+    
+
+
++    private String model;
+    
+
+
++    private List<?> messages;
+    
+
+
++    private int n;
+    
+
+
++    private double temperature;
+    
+
+
++
+    
+
+
++    public boolean newMethod(String st1, String st2) {
+                +        if (st1 == st2) {
+            
+
+
+                    +            return true;
+            
+
+
+                    +        }
+                +        return false;
+                +    }
+    
+
+
++}
