f84665b06ee53cb15011a686c5f4b116d28305c5
BEL HADJ ALI SeifEddine <belhadjali.seif@gmail.com>
2024-09-28 09:58:48 +0200

    add new file

---
+++ src/main/java/ChatReques10.java
@@ -0,0 +1,27 @@
+import lombok.*;
+
+import java.util.List;
+
+/**
+ * @author madhankumar
+ */
+@Setter
+@Getter
+@ToString
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatReques10 {
+    private String model;
+    private List<?> messages;
+    private int n;
+    private double temperature;
+
+    public boolean newMethod(String st1, String st2) {
+        if (st1 == st2) {
+            return true;
+        }
+        return false;
+    }
+}
